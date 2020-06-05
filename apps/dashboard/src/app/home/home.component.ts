dc9c486428591a79319efba807f7be97966e76b8
nilshah98 <neel12@somaiya.edu>
2020-06-05 22:09:21 +0530

    [FEAT] (Home) Component, View created

--- apps/dashboard/src/app/home/home.component.ts
+++ apps/dashboard/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService, User } from '@workspace/core-data';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  data: any = [
+    {icon: "work", title:"Course", link: "/projects",
+    line1: "Teach anything you wish to using our course feature",
+    line2: "Users can view, create, edit & delete courses"},
+    
+    {icon: "help", title:"Questions", link: "/questions",
+    line1: "Add & view questions without the constraint of a quiz",
+    line2: "Users can add dynamic number of options"},
+
+    {icon: "dashboard", title:"Quiz", link: "/quiz",
+    line1: "Create & view quizes created by teachers from everywhere",
+    line2: "Users can create quiz by choosing questions from our pool"},
+    
+    {icon: "question_answer", title:"Exam", link: '/exam',
+    line1: "Give exams, and evaluate your results. Get percentile & scorecard.",
+    line2: "Bonus all of the above resources are searchable as well"},
+  ]
+
+  currUser: User;
+
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.getUserDetails()
+      .subscribe(res => {
+        this.currUser = res;
+      })
+  }
 
   ngOnInit(): void {
+  }
+
+  goToPage(pageName:string){
+    this.router.navigate([`${pageName}`]);
   }
 
 }
--- apps/dashboard/src/app/home/home.module.ts
+++ apps/dashboard/src/app/home/home.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MaterialModule } from '@workspace/material';
 
 
 @NgModule({
   declarations: [HomeComponent],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MaterialModule
   ],
   exports: [
     HomeComponent
